d5065db8ecde66c87be8d7785073e8824312b385
Pixeled <86133616+Pixeled99@users.noreply.github.com>
2024-08-27 12:22:20 -0500

    Add files via upload

---
+++ Canvas Confetti-2024-08-27.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Canvas Confetti
+// @namespace    http://github.com/Pixeled99/CanvasConfetti
+// @version      2024-08-27
+// @description  Brighten up your canvas experience with more confetti on your assignments!
+// @author       Pixeled99
+// @match        https://nscds.instructure.com/courses/*/assignments/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instructure.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var url = window.location.href;
+    if (!url.includes("confetti=true")) {
+        if (url.includes("?")){
+            location.replace(url + "&confetti=true")
+        }
+        else {
+            location.replace(url + "?confetti=true")
+        }
+    }
+})();
